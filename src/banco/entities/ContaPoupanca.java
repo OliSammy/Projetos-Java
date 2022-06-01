313d64f63663d8157e54097918f4b8acc573bff1
Matheus Freitas <freitas.matheus.profissional@outlook.com>
2022-06-01 09:36:14 -0300

    Classe filha de conta, ContaCorrente, criada.

---
+++ src/banco/entities/ContaPoupanca.java
@@ -0,0 +1,41 @@
+package banco.entities;
+
+import banco.domainexception.*;
+import banco.services.*;
+
+public class ContaPoupanca extends Conta implements MetodosContas {
+    private TaxaRendimento rendimento;
+
+    public ContaPoupanca(String titular, int idConta, double saldo, TaxaRendimento taxa){
+        super(titular, idConta, saldo);
+        rendimento = taxa;
+    }
+
+    @Override
+    public void depositar(double valor) throws NumeroNegativoException {
+        if (valor < 0) {
+            throw new NumeroNegativoException();
+        }
+        saldo += valor;
+        
+    }
+
+    @Override
+    public void sacar(double valor) throws NumeroNegativoException, SaldoIndisponivelException {
+        if(valor>saldo){
+            throw new SaldoIndisponivelException();
+        }
+        saldo-=valor;        
+    }
+
+    @Override
+    public void transferir(Conta destino, double valor)
+            throws NumeroNegativoException, SaldoIndisponivelException, ContaInexistenteException {
+        if(destino == null){
+            throw new ContaInexistenteException();
+        }
+        this.sacar(valor);
+        destino.saldo+=valor;
+    }
+    
+}
